4ffa61f20f6859554159b60973d7cb4fea3cf2cb
Prateek <rajprateek53@gmail.com>
2023-10-02 02:41:11 +0530

    initial commit

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,8 @@
+import styles from "./Footer.module.css";
+
+const Footer = () => {
+  return (
+    <div className={styles.footerSection}>Made with 💖 by Prateek Raj</div>
+  );
+};
+export default Footer;
